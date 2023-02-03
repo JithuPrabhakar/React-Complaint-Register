245704fd7856fcc30d4988b95544a4bc7c9f248a
Jithu Prabhakar <join.jitz@gmail.com>
2023-02-03 11:46:00 +0530

    new changes

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import '../styles/Home.css'
+import homePage from '../images/homePage.jpg'
 
 const Home = () => {
   return (
     <div className='container content'>
-      <h1 className='text-center'>Home</h1>
-      <h3>What is Lorem Ipsum?</h3>
-      <h4>"Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."</h4>
-      <h5>"There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain..."</h5>
-      <h2>What is Lorem Ipsum?</h2>
+      <h1 className='text-center'>Build A Home</h1>
+      <h3>Build The House of your Dreams</h3>
+      <img class="home-img" src={ homePage } alt='Home'/>
+      <h4>Home Page description</h4>
+      <h2>What We Do</h2>
       <p><strong>Lorem Ipsum</strong> is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
     </div>
   )
